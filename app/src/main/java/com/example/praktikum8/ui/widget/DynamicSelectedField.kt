3914c1d014d9596bb0fbcd79f360e1f033f58ab0
safirasn21 <safirasn021@gmai.com>
2024-11-22 14:19:52 +0700

    Menambahkan OutlinedTextField dengan ikon dropdown dan mode readOnly

--- app/src/main/java/com/example/praktikum8/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/praktikum8/ui/widget/DynamicSelectedField.kt
@@ -25,13 +25,30 @@ fun DynamicSelectedField(
     label: String,
     onValueChangeEvent: (String) -> Unit,
     modifier: Modifier = Modifier
-) {
-    var expanded by remember {
-        mutableStateOf(false)
+){
+    var expanded by remember { mutableStateOf(false)
     }
-    ExposedDropdownMenuBox(
+    ExposedDropdownMenuBox (
         expanded = expanded,
-        onExpandedChange = { expanded = !expanded },
+        onExpandedChange = { expanded = !expanded},
         modifier = modifier
-    ){}
+    ) {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+
+
+    }
 }
