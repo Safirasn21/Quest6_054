fef8aa537761ee3fe2129172fcb1a6f23dccc231
safirasn21 <safirasn021@gmai.com>
2024-11-22 16:33:00 +0700

    memperbaiki code yang error

--- app/src/main/java/com/example/praktikum8/ui/viewModel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/praktikum8/ui/viewModel/RencanaStudyViewModel.kt
@@ -12,14 +12,14 @@ class RencanaStudyViewModel : ViewModel() {
     val krsStateUi : StateFlow<RencanaStudy> = _krsState.asStateFlow()
 
     fun setMataKuliah(mkPilihan: String){
-        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan)
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan)
         }
         fun setKelas(kelasPilihan: String){
          _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
      }
        fun saveDataKRS(ls: MutableList<String>){
            _krsState.update { status -> status.copy(
-               namaMk = ls[0],
+               mataKuliah = ls[0],
                kelas = ls[1]
            ) }
        }
