3953429ba03c91c3db360fe4d6b9345281ea842f
safirasn21 <safirasn021@gmai.com>
2024-11-22 15:53:08 +0700

    menambahkan fungsi rencanastudyview dan menambahkan var listdata

--- app/src/main/java/com/example/praktikum8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum8/ui/screen/RencanaStudyView.kt
@@ -1,11 +1,45 @@
 package com.example.praktikum8.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.praktikum8.R
+import com.example.praktikum8.data.MataKuliah
+import com.example.praktikum8.data.RuangKelas
 import com.example.praktikum8.model.Mahasiswa
+import com.example.praktikum8.ui.widget.DynamicSelectedField
 
 @Composable
 fun RencanaStudyView(
@@ -16,4 +50,8 @@ fun RencanaStudyView(
     var chosenDropdown by remember { mutableStateOf(" ") }
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+
+
+
 }
