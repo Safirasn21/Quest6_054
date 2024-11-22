957c21b58fe510f0fce4811842c924ccd1b31db0
safirasn21 <safirasn021@gmai.com>
2024-11-22 14:07:09 +0700

    menambahkan state untuk nama, NIM, dan email dengan MutableState

--- app/src/main/java/com/example/praktikum8/ui/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/praktikum8/ui/screen/MahasiswaFormView.kt
@@ -44,6 +44,14 @@ fun MahasiswaFormView(
     modifier: Modifier,
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
-) {}
+) {
+    var nama by remember { mutableStateOf("") }
+    var nim by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    val listData: MutableList<String> =
+        mutableListOf(nim, nama, email)
+
+}
 
 
