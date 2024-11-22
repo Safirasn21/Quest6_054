c00cd6f642bc19c5d00e108c0f1334514e950d4a
safirasn21 <safirasn021@gmai.com>
2024-11-22 13:56:09 +0700

    menambahkan objek untuk MataKuliah dengan daftar pilihan mata kuliah

---
+++ app/src/main/java/com/example/praktikum8/data/MataKuliah.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum8.data
+
+object MataKuliah {
+    val options = listOf(
+        "Agama Islam dan Kemuhammadiyahan",
+        "Pengembangan Aplikasi WEB",
+        "Dasar Teknologi Informasi",
+        "Fisika dan Elektronika",
+        "Logika Teknik Pemogramman",
+    )
+}
