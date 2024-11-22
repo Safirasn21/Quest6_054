f7f852cd93c71c8d436414794ed8492bdd8fd178
safirasn21 <safirasn021@gmai.com>
2024-11-22 15:54:50 +0700

    menambahkan box dengan shape roundedcorner

--- app/src/main/java/com/example/praktikum8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum8/ui/screen/RencanaStudyView.kt
@@ -90,6 +90,15 @@ fun RencanaStudyView(
                )
            }
        }
-
+        Box(
+            modifier = Modifier.background(
+                color = Color.White,
+                shape = RoundedCornerShape(
+                    topEnd = 15.dp,
+                    topStart = 15.dp
+                )
+            )
+                .fillMaxSize(),
+        )
     }
 }
